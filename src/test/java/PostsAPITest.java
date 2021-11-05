12d0b4aed7ffa54df7db9b86602d9e10039ab5bc
Ravi <ravi.singh.17@mountblue.tech>
2021-11-05 09:19:31 +0530

    Create new user using post method tested

--- src/test/java/PostsAPITest.java
+++ src/test/java/PostsAPITest.java
@@ -5,12 +5,27 @@ import static org.hamcrest.Matchers.equalTo;
 public class PostsAPITest {
 
     @Test
-    public void testListAllUser() {
+    public void testListAllPosts() {
             given()
                 .when()
                     .get("https://jsonplaceholder.typicode.com/posts")
                 .then()
                     .body("[0].id",equalTo(1))
                     .statusCode(200);
+    }
+
+    @Test
+    public void testCreateNewPost() {
+        baseURI = "https://jsonplaceholder.typicode.com/posts";
+        String newPost = "{"+     "\"userId\":"+ "1,"+
+                "\"title\":"+ "Felling happy,"+
+                "\"body\":"+ "Extreme joy and happiness"+
+    "}";
+        given().body(newPost)
+                .when()
+                    .post()
+                .then().log().all()
+                    .assertThat()
+                    .statusCode(201);
     }
 }
